91c9b15a9d0360d7fdf8bd7d9a1149371688df8b
GauravPatel <69468357+gauravpatel0512@users.noreply.github.com>
2022-09-03 16:09:27 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DragNDrop from "./components/DragNDrop";
+import "./App.css";
+
+const data = [
+	{ title: "Group 1", items: ["1", "2", "3"] },
+	{ title: "Group 2", items: ["4", "5"] },
+	{ title: "Group 3", items: ["6", "7", "8"] },
+	{ title: "Group 4", items: ["9", "10", "11", "12"] },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<DragNDrop data={data} />
+			</header>
+		</div>
+	);
 }
 
 export default App;
